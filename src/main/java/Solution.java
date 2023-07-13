78fb51a1ee4af9beebb2d84d9e595821def712ca
alek <wait82985@gmail.com>
2023-07-13 14:51:10 +0600

    problem solved. Dijkstra algorithm

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,67 @@
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 public class Solution {
     public int swimInWater(int[][] grid) {
-        //TODO
-        return 0;
+
+        PriorityQueue<Cell> priorityQueue = new PriorityQueue<>(Comparator.comparing(a -> a.weight));
+
+        priorityQueue.offer(new Cell(0, 0, grid[0][0]));
+
+        Set<String> visited = new HashSet<>();
+
+        int result = 0;
+
+        int[][] directions = {
+                {-1, 0},
+                {1, 0},
+                {0, -1},
+                {0, 1}
+        };
+
+        while (!priorityQueue.isEmpty()){
+            Cell current = priorityQueue.poll();
+
+            int currentI = current.i;
+            int currentJ = current.j;
+            int currentWeight = current.weight;
+
+            result = Math.max(result, currentWeight);
+
+            if (currentI == grid.length - 1 && currentJ == grid.length - 1) {
+                break;
+            }
+
+            if(visited.contains(currentI + " " + currentJ)){
+                continue;
+            }
+            visited.add(currentI + " " + currentJ);
+
+            for (int[] direction : directions) {
+                int nextI = currentI + direction[0];
+                int nextJ = currentJ + direction[1];
+                if(nextI >= 0 && nextI < grid.length
+                        && nextJ >= 0 && nextJ < grid.length
+                        && !visited.contains(nextI + " " + nextJ)){
+                    priorityQueue.offer(new Cell(nextI, nextJ, Math.max(currentWeight, grid[nextI][nextJ])));
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+class Cell{
+    int i;
+    int j;
+    int weight;
+
+    public Cell(int i, int j, int weight) {
+        this.i = i;
+        this.j = j;
+        this.weight = weight;
     }
 }
